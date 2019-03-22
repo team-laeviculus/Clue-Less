918eebcc2bc0ee5087b641968b1401e410706f4e
Taylor Pearlman <tpearlman@localhost.localdomain>
2019-03-22 13:17:15 -0400

    Add additional player db functionality

--- Networking/client.py
+++ Networking/client.py
@@ -1,11 +1,31 @@
 import requests
 
-server_url = 'http://127.0.0.1:5000/'
+class Client():
 
-def get_all_players():
-    r = requests.get(server_url + 'players')
-    print(r.url)
-    print(r.json())
+    def __init__(self, url, name):
+        self.server_url = url
+        self.name = name
+
+    def get_all_players(self):
+        r = requests.get(self.server_url + 'players')
+        print(r.json())
+
+    def join_game(self):
+        data = {'name': self.name}
+        r = requests.post(self.server_url + 'players', json=data)
+
+    def leave_game(self):
+        data = {'name': self.name}
+        r = requests.delete(self.server_url + 'players', json=data)
+
+  #  def end_game(self):
+  #      r = requests.get(self.server_url + 'end_game')
 
 if __name__ == "__main__":
-    get_all_players()
+    server_url = 'http://127.0.0.1:5000/'
+    client = Client(server_url, 'test_name')
+    client.get_all_players()
+    client.join_game()
+    client.get_all_players()
+    client.leave_game()
+    client.get_all_players()
--- Networking/server.py
+++ Networking/server.py
@@ -1,24 +1,46 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 import sqlite3
 from flask_restful import Resource, Api
 
 # Use dictionaries instead of lists for database returns
+# This is optional
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
 
-
+# Access to the player database
+# Posts new players to the database, gets lists of all active players
 class HandlePlayers(Resource):
     def get(self):
-        conn = sqlite3.connect('players.db')
+        conn = sqlite3.connect('../Databases/players.db')
         conn.row_factory = dict_factory
         cur = conn.cursor()
         all_players = cur.execute('SELECT * FROM players;').fetchall()
         return jsonify(all_players)
 
+    def post(self):
+        conn = sqlite3.connect('../Databases/players.db')
+        cur = conn.cursor()
+        values = [request.json["name"]]
+        command = 'INSERT INTO players(name) VALUES(?)'
+        cur.execute(command, values)
+        conn.commit()
+        conn.close()
+
+    def delete(self):
+        conn = sqlite3.connect('../Databases/players.db')
+        cur = conn.cursor()
+        values = [request.json["name"]]
+        command = 'DELETE FROM players WHERE name = ?'
+        cur.execute(command, values)
+        conn.commit()
+        conn.close()
+
+def end_game():
+    return(jsonify("Ending Game"))
 
 def go():
     app = flask.Flask(__name__)
@@ -26,6 +48,9 @@ def go():
     api = Api(app)
 
     api.add_resource(HandlePlayers, '/players')
+    api.add_resource(end_game, '/end_game')
+
+
     app.run()
 
 if __name__ == "__main__":
