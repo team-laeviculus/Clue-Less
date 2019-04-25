de70412aae2a254ad8e1b5afe8d787c2593bcda1
v3nturetheworld <pheinenjr@gmail.com>
2019-04-25 01:42:05 -0500

    almost there (tm)

--- KISSClient.py
+++ KISSClient.py
@@ -5,8 +5,11 @@ Keep It Simple Stupid....
 import cmd
 import requests
 import sys
+import shlex
 import time
-
+import traceback
+from collections import OrderedDict
+from http import HTTPStatus
 class Player:
     local_info = {
         "name": None,
@@ -21,6 +24,42 @@ class ServerInfo:
         "turn": None
     }
     address = "http://127.0.0.1:5000/games"
+
+
+class LocalGameInfo:
+    # Hallways
+    HALLWAYS = ["study_hall",
+                "hall_lounge"
+                "library_billard room",
+                "billard room_dinning room",
+                "conservatory_ballroom",
+                "ballroom_kitchen",
+                "study_library",
+                "hall_billard room",
+                "lounge_dining room",
+                "library_conservatory",
+                "billard room_ballroom",
+                "dining room_kitchen",
+    ]
+    HALLWAYS_MAP = OrderedDict({k: v for k,v in enumerate(HALLWAYS)})
+
+    ROOMS = [
+        'Kitchen',
+        'Conservatory',
+        'Dining Room',
+        'Ballroom',
+        'Study',
+        'Hall',
+        'Lounge',
+        'Library',
+        'Billard Room',
+    ]
+
+    ROOM_MAP = OrderedDict({k: v for k,v in enumerate(ROOMS)})
+
+
+    def __init__(self):
+        pass
 
 
 class CommandShell(cmd.Cmd):
@@ -59,7 +98,7 @@ class CommandShell(cmd.Cmd):
         sys.exit(0)
 
 
-    def wait_for_my_turn(self, tick=0.1):
+    def wait_for_my_turn(self, tick=0.4):
         """
         The loop that listens for other players turns after a player makes a turn
         Breakes when inbound_q has a message saying its my turn
@@ -71,6 +110,8 @@ class CommandShell(cmd.Cmd):
             data = Player.local_info
             data['request'] = 'get_next_turn'
             r = requests.get(ServerInfo.address, json=data)
+            if r.status_code == 500:
+                print(f"ERROR WITH REQUEST")
             print(f"response: {r}")
             r_data = r.json()
             print(r.text)
@@ -80,11 +121,77 @@ class CommandShell(cmd.Cmd):
             # if 'turn' in r_data and r_data['turn'] == Player.local_info['token']:
             if 'turn' in r_data and r_data['turn']['name'] == Player.local_info['name']:
                 print("Its my turn!!")
-                break
+                self.ask_player_for_move()
+                # Get Game Board:
                 # input()
                 # request.put
                 # parse response
             time.sleep(tick)
+
+
+    ###################
+    ## stupid parser
+    ###################
+    def move_to_loc(self, args=None):
+        print(f"move {args}")
+        # Do some client side validation
+        for k,v in LocalGameInfo.ROOM_MAP.items():
+            print(f"{k}: {v}")
+        room = input("make a choice: ")
+        print(f"You CHose: {room}")
+        parsed_r = int(room)#shlex.split(room)
+        print(f"PArsed: {parsed_r}")
+        if room and int(room) in LocalGameInfo.ROOM_MAP:
+            return {
+                "move_to_location": LocalGameInfo[(room[0])]
+            }
+        return None
+
+    def make_suggestion(self, args=None):
+        print(f"suggest: {args}")
+        return True
+
+    def make_accuse(self, args=None):
+        print(f"accuse: {args}")
+        return True
+
+    def ask_player_for_move(self):
+        put_request = None
+        moves = OrderedDict({
+            1: ("Move To a Location", self.move_to_loc),
+            2: ("Make a Suggestion", self.make_suggestion),
+            3: ("Make an Accusation", self.make_accuse)
+        })
+
+
+        print("Choose A Move To Make")
+        for k,v in moves.items():
+            print(f"{k}: {v[0]}")
+
+        response = input("Choose A Move To Make> ")
+        print(f"REsponse: {response}")
+        if response is 'q' or response is 'quit':
+            sys.exit()
+        else:
+            try:
+                parsed_r = shlex.split(response)
+                num_r = int(parsed_r[0])
+                print(f"Num: {num_r}")
+                if num_r in moves:
+                    put_request = moves[num_r][1](parsed_r)
+            except:
+                print(f"Error! Invalid response {parsed_r}")
+                self.ask_player_for_move()
+                traceback.print_exc()
+        req = dict(Player.local_info)
+        req['request'] = put_request
+        r = requests.put('http://127.0.0.1:5000' + '/games', json=req)
+        print(f'server reply: {r.text}')
+
+
+
+
+
 
 c = CommandShell()
 c.cmdloop()
--- Networking/KISServer.py
+++ Networking/KISServer.py
@@ -5,6 +5,12 @@ Keep It Simple Stupid.....
 
 from flask import Flask, request, jsonify
 from collections import OrderedDict
+import random
+import threading
+import traceback
+from Databases.db_mgmt import CluelessDB
+from ClueGameBoard.GameBoard_DatabaseCoordinates import GameBoard
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -14,6 +20,20 @@ class GameState:
     GAME_VER = "GAME_OVER"
 
     CURRENT_STATE = WAITING_FOR_PLAYERS
+
+def create_game_tokens():
+    #TODO: We might need to do more with this
+    # Token_Name, Player Associated
+    tokens_map = {
+        "Prof Plum" : None,
+        "Mrs. Peacock" : None,
+        "Mr. Green" : None,
+        "Mrs. White" : None,
+        "Col. Mustard" : None,
+        "Miss Scarlet" : None
+
+    }
+    return tokens_map
 
 class Player:
 
@@ -25,7 +45,13 @@ class Player:
             "location": None
         }
     def get_player(self):
-        return  self.data
+        return self.data
+
+    def set_token(self, token):
+        self.data['token'] = token
+
+    def set_location(self, location):
+        self.data['location'] = location
 
 class GameInfo:
     game = {
@@ -37,7 +63,102 @@ class GameInfo:
     current_players_turn = 0 # Player dict
     players_turn_name = None
 
-@app.route("/games",methods=["GET","POST","PUT"])
+class ClueLessCommon:
+    """
+    DB Controller and game board. Initialized once
+    """
+    db_controller = None
+    game_board = None
+    CLUELESS_MUTEX = None
+    PLAYER_RANDOM_TOKENS = None
+
+    TOKENS = [
+        "Prof Plum",
+         "Mrs. Peacock",
+        "Mr. Green",
+        "Mrs. White",
+        "Col. Mustard",
+        "Miss Scarlet"
+    ]
+    PLAYER_TOKEN_MAP = OrderedDict()
+
+    @staticmethod
+    def initialize():
+        if not (ClueLessCommon.db_controller and ClueLessCommon.game_board):
+            ClueLessCommon.db_controller = CluelessDB()
+            ClueLessCommon.db_controller.create_games_table()
+            ClueLessCommon.db_controller.create_player_table()
+            ClueLessCommon.game_board = GameBoard.create_game_board(ClueLessCommon.db_controller, print_board=True)
+            ClueLessCommon.CLUELESS_MUTEX = threading.Lock()
+            print(ClueLessCommon.TOKENS)
+            ClueLessCommon.PLAYER_RANDOM_TOKENS = random.sample(ClueLessCommon.TOKENS, len(ClueLessCommon.TOKENS))
+            print(ClueLessCommon.PLAYER_RANDOM_TOKENS)
+
+
+    @staticmethod
+    def start_game():
+
+        ClueLessCommon.db_controller.create_suspect_table()
+        ClueLessCommon.db_controller.init_suspects(1)
+        ClueLessCommon.db_controller.create_weapon_table()
+        ClueLessCommon.db_controller.init_weapons(1)
+        ClueLessCommon.db_controller.create_room_table()
+        ClueLessCommon.db_controller.init_rooms(1)
+        ClueLessCommon.db_controller.create_cards_table()
+        ClueLessCommon.db_controller.init_cards(1)
+        ClueLessCommon.db_controller.create_notebook_table()
+
+        # initialize case file
+        ClueLessCommon.db_controller.create_case_file_table()
+
+        # establish the solution for a game
+        solution_s = random.randint(1, 6)
+        solution_w = random.randint(7, 12)
+        solution_r = random.randint(13, 21)
+
+        ClueLessCommon.db_controller.init_case_file(1, solution_s, solution_w, solution_r)
+
+        ClueLessCommon.db_controller.update_suspects(1, solution_s)
+        ClueLessCommon.db_controller.update_weapons(1, solution_w)
+        ClueLessCommon.db_controller.update_rooms(1, solution_r)
+        ClueLessCommon.db_controller.shuffle_deal_cards(1, GameInfo.game['player_count'], solution_s, solution_w, solution_r)
+        ClueLessCommon.db_controller.create_suggest_log_table()  # only do this at the beginning of the game
+        ClueLessCommon.db_controller.create_accuse_log_table()
+
+
+    HALLWAYS = ["study_hall",
+                "hall_lounge"
+                "library_billard room",
+                "billard room_dinning room",
+                "conservatory_ballroom",
+                "ballroom_kitchen",
+                "study_library",
+                "hall_billard room",
+                "lounge_dining room",
+                "library_conservatory",
+                "billard room_ballroom",
+                "dining room_kitchen",
+    ]
+
+    HALLWAYS_MAP = OrderedDict({k: v for k, v in enumerate(HALLWAYS)})
+
+    ROOMS = [
+        'Kitchen',
+        'Conservatory',
+        'Dining Room',
+        'Ballroom',
+        'Study',
+        'Hall',
+        'Lounge',
+        'Library',
+        'Billard Room',
+    ]
+
+    ROOM_MAP = OrderedDict({k: v for k, v in enumerate(ROOMS)})
+
+
+
+@app.route("/games", methods=["GET", "POST", "PUT"])
 def get_game_info():
     content = request.get_json()
     #print(f"[{request.method}]:New Request:  {content}")
@@ -68,17 +189,27 @@ def get_game_info():
 
 def handle_player_join(rquest_data):
     #TODO join
+    #TODO Name already in use error
+    #TODO game full error
     print(f"Handle player joinm: {rquest_data}")
     print(rquest_data['name'])
-    GameInfo.game['players'][rquest_data['name']] = Player(rquest_data['name']).get_player()
+    p = Player(rquest_data['name']).get_player()
+    p['token'] = ClueLessCommon.PLAYER_RANDOM_TOKENS.pop()
+    ClueLessCommon.PLAYER_TOKEN_MAP[p['token']] = p['name']
+    ClueLessCommon.db_controller.put_player_in_game(p['name'])
+    GameInfo.game['players'][rquest_data['name']] = p
     GameInfo.game['player_count'] += 1
     if GameInfo.game['player_count'] >= 2:
         GameState.CURRENT_STATE = GameState.GAME_RUNNING
+        ClueLessCommon.start_game()
         print("Game Full STARTING!!")
-        get_next_turn()
+        #GameInfo.players_turn_name = get_next_turn()
+        print(f"First Player is: {GameInfo.players_turn_name}")
     print(f"New Game info: {GameInfo.game}")
     response_msg = {
-                "game_state": GameState.WAITING_FOR_PLAYERS
+                "game_state": GameState.WAITING_FOR_PLAYERS,
+                "token": p['token'],
+                "location": p['location']
             }
     return response_msg
 
@@ -87,12 +218,17 @@ def handle_game_running_request(request_data):
     print(f"game_running_request: {request_data}")
     player = request_data['name']
     player_token = request_data['token']
+    if GameInfo.players_turn_name is None:
+        print("players turn is none")
+        GameInfo.players_turn_name = list(GameInfo.game['players'].items())[0]
+        print(f"There was no player set for first turn. Now it is: {GameInfo.players_turn_name}")
     if GameInfo.players_turn_name == player:
         turn_info = request_data['request']
         # TODO: Update Game Board
+        print(f"Turn Info: {turn_info}")
 
         return {"turn": GameInfo.players_turn_name[1]}
-
+    print(f"player whose turn it is: {GameInfo.players_turn_name}")
     return {"turn": GameInfo.players_turn_name[1]}
 
 def get_next_turn():
@@ -101,22 +237,35 @@ def get_next_turn():
 
     :return: the self.player[<playername>] object of player whose turn it is
     """
+    ClueLessCommon.CLUELESS_MUTEX.acquire()
+
     print("get_next_turn")
     if GameState.CURRENT_STATE == GameState.GAME_RUNNING:
         this_players_turn = list(GameInfo.game['players'].items())[GameInfo.current_players_turn]
-        # this_players_turn[1]["my_turn"] = True
-        print(f"New Player Turn: {this_players_turn}")
-        GameInfo.current_players_turn = (GameInfo.current_players_turn + 1) % GameInfo.game['player_count']
-        GameInfo.players_turn_name = this_players_turn
-        # TODO: Notify player its their turn
-        print(f"Player: {this_players_turn} next")
+        try:
+            # this_players_turn[1]["my_turn"] = True
+            print(f"New Player Turn: {this_players_turn}")
+            GameInfo.current_players_turn = (GameInfo.current_players_turn + 1) % GameInfo.game['player_count']
+            GameInfo.players_turn_name = this_players_turn
+            print(f"Player: {this_players_turn} next")
+
+            # TODO: Notify player its their turn
+            ClueLessCommon.db_controller.update_active_turn(this_players_turn[0])
+            print("DB stuff done")
+        except:
+            traceback.print_exc()
+        finally:
+            ClueLessCommon.CLUELESS_MUTEX.release()
 
         return this_players_turn
     # log.info("Game state is not ready to return a players turn")
     return False
 
 
+"""
+Initialize server constants
+"""
 
 
-
+ClueLessCommon.initialize()
 app.run()
